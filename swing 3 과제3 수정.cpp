b7e9882e9013b9b280e4a2125fe9569f6520242e
hyojeongchoi <63584457+hyojeongchoi@users.noreply.github.com>
2020-06-02 13:58:44 +0900

    Add files via upload

---
+++ swing 3 과제3 수정.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+void sort(int* number, int size)
+{
+    int i = 0, j = 0, temp = 0;
+
+    for (j = 0; j < size; j++)
+    {
+        for (i = 0; i < size - 1; i++)
+        {
+            if (number[i] > number[i + 1])
+            {
+                temp = number[i];
+                number[i] = number[i + 1];
+                number[i + 1] = temp;
+            }
+        }
+    }
+}
+
+void print(int* number, int size) 
+{
+    for (int j = 0; j < 5; j++)
+    {
+        cout << j + 1 << ". " << number[j] << '\n';
+    }
+}
+
+int main(void)
+{
+	int number[5];
+	int i = 0;
+
+	cout << "숫자 다섯개를 입력해주세요 (띄어쓰기로 구분) >>";
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> number[i];
+	}
+    cout << "< 정렬 전 >\n";
+    print(number, 5);
+    cout << "\n";
+
+    cout << "< 정렬 후 >\n";
+    sort(number, 5);
+    print(number, 5);
+
+}
